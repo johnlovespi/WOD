560d4e38d50fb451054d5eb04ff89ad1a781ff95
john espinoza <johne3741@gmail.com>
2016-10-05 16:31:48 -0400

    patient and google is progress

--- player.js
+++ player.js
@@ -2,12 +2,12 @@ console.log('working');
 
 
 $(document).ready(function() {
-
   $(document).keydown(function(e){
     switch (e.which) {
-    case 38:
-        if(parseInt($('.player').css('top')) > -290){
-        $('.player').stop().animate({top: '-=100'});
+    case 38://
+    var p = Math.floor(parseInt($('.player').css('top')));
+        if( p > -230){
+        $('.player').stop().animate({top: '-=150'});
         //left arrow key
           // console.log('working');
             // $(".player").top('-310');
@@ -20,31 +20,61 @@ $(document).ready(function() {
         // if($('.player') > ){
             // console.log($('.player').position());
         }
-
-        break;
-}
-
-})
+      break;
+    }
+  })
 });
 
 // $(function) {
-function move() {
-    $('.roll-blocks').animate({right: '+=1000px'}, 1000)
-    // if('.roll-blocks')
-     $('.roll-blocks2').animate({right: '+=1000px'}, 1000)
-      // console.log('.roll-blocks').position;
+// function move() {
+//     $('.roll-blocks').animate({right: '+=1000px'}, 1000)
+//           // console.log($('.roll-blocks').right;
+//     // if('.roll-blocks')
+//      $('.roll-blocks2').animate({right: '+=1000px'}, 1000)
+//       // console.log('.roll-blocks').position;
 
-};
-setInterval(move, 1000);
+// };
+// setInterval(move, 1000);
 
 
-// function myFunction() {
-//
+
+$(function  move() {
+  setInterval(function() {
+    $('.roll-blocks').animate({right: $(window).width() + 'px' }, 2000, 'linear', function() {
+      $('.roll-blocks').css({ right: - 0})
+      // .css({top: 49 + 'px'}).randTopbox();
+
+
+
+      // $('.roll-blocks').css({top: + randomTop()});
+        // console.log($('.roll-blocks').width())
+  });
+}, 1000);
+});
+
+$(function  move() {
+  setInterval(function() {
+    $('.roll-blocks2').animate({right: $(window).width() + 'px' }, 2000, 'linear', function() {
+      $('.roll-blocks2').css({ right: - 0})
+      // .css({top: 49 + 'px'}).randTopbox();
+      // $('.roll-blocks').css({top: + randomTop()});
+        // console.log($('.roll-blocks').width())
+  });
+}, 1000);
+});
+
+
+//make a function to give div randome top
+// function randomTop(){
+//   var randTopbox = Math.floor(Math.random()*100);
+//    $('.roll-blocks').css({'margin-top': randTopbox+'px'});
+
 // }
 
+// setInterval(randomTop, 1000);
 
- // $('.roll-blocks2').animate({right: '+=1000px'}, 1000)
- //      console.log($('.roll-blocks').position());
+
+
 
 
 
